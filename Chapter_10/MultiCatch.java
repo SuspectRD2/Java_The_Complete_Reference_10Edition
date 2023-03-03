4acfaa69740e0680602eef69801ad16c4baa4091
Suspect/Sus/Сус <114314026+SuspectRD2@users.noreply.github.com>
2023-03-03 19:12:29 +0700

    Add files via upload

---
+++ Chapter_10/MultiCatch.java
@@ -0,0 +1,18 @@
+// Демонстрация сногократного перехвата
+
+public class MultiCatch {
+    public static void main(String args[]) {
+        int a = 10, b = 0;
+        int vals[] = { 1, 2, 3 };
+        
+        try {
+            int result = a / b;     // сгенерировать исключение типа ArithmeticException
+            // vals[10] = 19;       // сгенерировать исключение типа ArrayIndexOutOfBoundsException
+            
+            // В этом операторе catch перехватываются оба исключения
+        } catch(ArithmeticException | ArrayIndexOutOfBoundsException e) {
+            System.out.println("Исключение перехвачено: " + e);
+        }
+        System.out.println("После многократного перехвата.");
+    }
+}
